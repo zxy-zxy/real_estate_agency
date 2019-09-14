2c462dd94f5339344ec60f8f95d6a78a27e1fa8e
ivan <sinitsinvanya@gmail.com>
2019-09-14 12:18:34 +0400

    Enable filter through new buildings on admin page

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at']
     list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
+    list_filter = ['new_building']
 
 
 admin.site.register(Flat, FlatAdmin)
